f742872d49b794783701d289d474619876051646
ScuidHD <67193202+ScuidHD@users.noreply.github.com>
2020-06-20 14:52:03 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1 Add")
+print("2 Subtract")
+print("3 Multiply")
+print("4 Divide")
+
+while True:
+    inp = input("Enter choice(1/2/3/4): ")
+    if inp in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if inp == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif inp == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif inp == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif inp == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Operation")
